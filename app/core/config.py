6776f5e95b486e5f8414467d98c87644cf5514cc
gc85-ua <gc85@alu.ua.es>
2026-09-17 23:45:09 +0200

    fix: check if env file exists

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -10,9 +10,9 @@ DATA_DIR.mkdir(parents=True, exist_ok=True)
 env_path = BASE_DIR / ".env"
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=env_path, env_file_encoding="utf-8")
-    cache_db_host: str # redis/valkey host
-    cache_db_port: int # redis/valkey port
-    cache_db: int # redis/valkey database number
+    model_config = SettingsConfigDict(env_file=env_path if env_path.exists() else None, env_file_encoding="utf-8", extra="ignore")
+    cache_db_host: str = 'localhost' # redis/valkey host
+    cache_db_port: int = 6379 # redis/valkey port
+    cache_db: int = 0 # redis/valkey database number
 
 settings = Settings()
